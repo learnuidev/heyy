05895430d8810de7ee31af9776a74d9d7ad09c01
learnuidev <learnuidev@gmail.com>
2025-01-08 15:43:25 -0500

    fix bug

--- lib/clerk/verify-session-token.ts
+++ lib/clerk/verify-session-token.ts
@@ -3,7 +3,11 @@ import { verifyToken } from "@clerk/clerk-sdk-node";
 export async function verifySessionToken(token: string) {
   const session = await verifyToken(token, {
     jwtKey: process.env.CLERK_JWT_KEY,
-    authorizedParties: ["http://localhost:3001", "mandarino.io"],
+    authorizedParties: [
+      "http://localhost:3001",
+      "https://www.mandarino.io",
+      "https://www.heyy.sh",
+    ],
   });
 
   return session;
